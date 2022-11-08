a269e912f69a4a641413de2a4b4194c8b6231d43
Somu88 <samikshyasapkota33@gmail.com>
2022-11-08 10:51:28 +0545

    construct

--- class-3.php
+++ class-3.php
@@ -1,15 +1,16 @@
 <?php
 class User
 {
-    public $username = 'Kushal';
-    protected $email = 'kushalamatya413@gmail.com';
-    public $password = '12345678';
+    public $username;
+    protected $email;
+    public $password;
 
-    // public function get_email()
-    // {
-    //     return $this->email;
-    // }
-
+    public function __construct($username, $email, $password)
+    {
+        $this->username = $username;
+        $this->email = $email;
+        $this->password = $password;
+    }
     public function set_email($email)
     {
         $this->email = $email;
@@ -24,19 +25,26 @@ class Admin extends User
 {
     public $auth = "super admin";
 
+
+    public function __construct($username, $email, $password, $auth)
+    {
+        parent::__construct($username, $email, $password);
+        $this->auth = $auth;
+    }
+
     public function get_email()
     {
         return $this->email;
     }
 }
 
-$usr1 = new User;
+$usr1 = new User('Kushal', 'xyz@gmail.com', '12345');
 $usr1->login();
 $usr1->set_email("xyz@gmail.com");
 // echo $usr1->get_email();
 echo "<br><br>";
 
-$admin = new Admin;
+$admin = new Admin('asdf', 'awe12', '122', 'ultra admin');
 $admin->username = "Administrator";
 $admin->login();
 $admin->set_email("admin@gmail.com");
